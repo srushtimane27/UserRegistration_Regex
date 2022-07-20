5181957215d06a960c581439db03da00784f8755
srushtimane27 <srushtimane31@gmail.com>
2022-07-20 16:25:24 +0530

    Welcome to User Registration

---
+++ src/Bridgelabz/UserRegistration.java
@@ -0,0 +1,21 @@
+package Bridgelabz;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserRegistration {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome in user registration program");
+        UserRegistration user = new UserRegistration();
+        user.firstname();
+    }
+    public boolean firstname(){
+        System.out.println("Enter First Name: ");
+        String name = sc.next();
+        boolean valid = name.matches("^[A-Z][a-z]{2,}");
+        System.out.println("First Name is: "+name +"\nvalid: " + valid);
+        return valid;
+
+    }
+}
